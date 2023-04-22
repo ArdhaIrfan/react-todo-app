e3ff978a92db15375ddd1f3f15fa3d6b4f3d2539
ArdhaIrfan <ardhairfan17@gmail.com>
2023-04-22 23:15:04 +0700

    Add CSS to components

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import Todos from './components/Todos';
 
 function App() {
@@ -26,11 +26,21 @@ function App() {
   console.log(todos)
 
   return (
-    <div>
-      <h1>My Todo List</h1>
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
       <Todos todos={todos} />
     </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
