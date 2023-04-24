9ee3b53d399a76cb6e17773fb3a385498b399c45
ArdhaIrfan <ardhairfan17@gmail.com>
2023-04-24 09:56:19 +0700

    useContext Hooks

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import Todos from './components/Todos'
 import TodoForm from './components/TodoForm'
+
+export const TodoContext = createContext()
 
 function App() {
   const [todos, setTodos] = useState([
@@ -55,12 +57,14 @@ function App() {
   }
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>My Todo List</h1>
-      {/* Teruskan function toggleCompleted ke component Todos */}
-      <TodoForm addTodo={addTodo}/>
-      <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo}/>
-    </div>
+
+    <TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        <TodoForm addTodo={addTodo} />
+        <Todos todos={todos} />
+      </div>
+    </TodoContext.Provider>
   )
 }
 
